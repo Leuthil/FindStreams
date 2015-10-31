e1cb03fea877c1737cbf97cdc36a6fbb233c3b19
Leuthil <leuthil@gmail.com>
2015-10-31 00:44:09 -0400

    Update addon.py

--- addon.py
+++ addon.py
@@ -1,11 +1,17 @@
-﻿import xbmcaddon
+import xbmc
+import xbmcaddon
 import xbmcgui
- 
-addon       = xbmcaddon.Addon()
-addonname   = addon.getAddonInfo('name')
- 
-line1 = "Hello World!"
-line2 = "We can write anything we want here"
-line3 = "Using Python"
- 
-xbmcgui.Dialog().ok(addonname, line1, line2, line3)
+
+addon = xbmcaddon.Addon()
+addonname = addon.getAddonInfo('name')
+icon = addon.getAddonInfo('icon')
+
+line1 = "This is a simple example of notifications"
+time = 5000 #in miliseconds
+
+intent = '"android.intent.action.VIEW"'
+netflixURL = 'https://www.netflix.com/watch/'
+movieId = '18171022'
+movieURL = '"'+netflixURL+movieId+'"'
+
+xbmc.executebuiltin('StartAndroidActivity(%s,%s,%s,%s)' % ("", intent, "", movieURL))
