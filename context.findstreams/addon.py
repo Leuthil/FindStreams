ecc0fbe480afd71d7e23bcebdc7877b93e4bad69
Leuthil <leuthil@gmail.com>
2016-02-19 11:37:16 -0500

    Delete addon.py

--- context.findstreams/addon.py
+++
@@ -1,6 +0,0 @@
-import sys
-import xbmc
- 
-if __name__ == '__main__':
-    message = "Clicked on '%s'" % sys.listitem.getLabel()
-    xbmc.executebuiltin("Notification(\"Hello context items!\", \"%s\")" % message)
